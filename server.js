35bd26456a59576fa86903e0ca0ffbfeaf516951
Tarun Kumar <kumar31tarun@gmail.com>
2024-12-17 22:08:51 +0530

    node fourth commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  response.send("Hello hello Mic check");
+  response.send("Hello hello Checking Mic");
 });
 
 app.listen(4000, "0.0.0.0", () => {
